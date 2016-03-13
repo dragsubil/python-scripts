62c7204ef4c748f7ad510413d35b24e9e043cff6
dragsubil <subil1407@gmail.com>
2016-03-13 23:24:53 +0530

    got it to save the webpages obtained from table.html

--- pagesaver/pagesaver.py
+++ pagesaver/pagesaver.py
@@ -2,39 +2,86 @@
 import re
 import urllib.request
 
-#function to extract link from the line
-
-fileno=1  #global variable
+#function to extract link and name from the line
 
 def linkExtract(file_line):
-	pattern1=re.compile(r'href=(.*?)>')         #regex patteren to look for the link in the "href='www.somewebsite.com/somelink.html'" part of each line
-	linktup=pattern1.search(file_line).groups()  #forms a tuple with one element containing the raw string link
-	link1=linktup[0]
-	link1=link1[1:-1]						    #removes the unwanted quotes from the ends
-	return link1
+
+	pattern1=re.compile(r'''		#regex patteren to seperate link, any tag inbetween link and name, and name
 	
-#function to parse each line to find link
+			href=					#start from the place where href is to get link
+			
+			(.*?)>					#obtaining the link from the line. '>' at the end specifies end of tag
+			
+			(<\D*>)?				#accounts for any extra tag that might appear inbetween link and title
+			
+			(.*?)<					#obtaining the name of the link present in the same line
+			
+			''', re.VERBOSE)        
+			
+	try:
+		link_and_name_tup=pattern1.search(file_line).groups()  #groups forms a tuple containing the elements obtained in the parantheses of the regex
+		
+	except AttributeError:										#in case of a line with none of the properties in regex, None is obtained in pattern.search
+		return													#this raises AttributeError, so we must got to the next line, hence return a None
+		
+	print(link_and_name_tup)
+	link1=link_and_name_tup[0]
+	name1=link_and_name_tup[2]									#name is actually the third element, second being a potential inbetween tag
+	link1=link1[1:-1]						   				 	#removes the unwanted quotes from the ends of the link we got 
+	return (link1,name1)
+	
+#function to parse each line in the html file and call the linkExtract()
+
 def fileParse(link_file):
-	for i in link_file:
-		link1=linkExtract(i)
-		pageSave(link1)                            #passes link to pageSave function
+	for i in link_file:	
+		try:													#because of the possibility of an AttributeError from linkExtract, we get a None returned 
+			link1,name1=linkExtract(i)							#this leads to a TypeError, which we try-exceptionise and go to next iteration
+			
+		except TypeError:									
+			continue
+			
+		print(link1,name1)
+		pageSave(link1,name1)                            #passes link and name to pageSave function
 
 		
-#NOTE: VERY IMPORTANT: change how the file name is obtained. maybe extract the name along with the link in the linkExtract function
-def pageSave(page_link):
-	file_name="pages/file{}.html".format(fileno)              
+#function to save the page with the extracted name, given by the extracted link 
+def pageSave(page_link,page_name):
+	file_name="pages/{}.html".format(page_name)              
 	with urllib.request.urlopen(page_link) as page_object:
 		with open(file_name,'ab+') as file1:
 			file1.write(page_object.read())
+			
 
+#combined unit test
+
+with open('tablesample.html','r+') as file1:
+	fileParse(file1)
+
+
+
+
+
+
+
+
+
+
+
+
+	
+#pageSave unit test
+'''
 testlink='http://www.diveintopython3.net/regular-expressions.html'
 
 pageSave(testlink)
-	
-	
-		
-		
-		
+'''
 
+#linkExtract unit test
+'''
+testfile_line='<div><strong><a title="View all posts filed under 11.e" href="https://parahumans.wordpress.com/category/stories-arcs-11/arc-11-infestation-stories-arcs-11/11-e/">11.e</a></strong></div>'
+	
+link1,name1=linkExtract(testfile_line)
 
+print(link1,name1)
+'''
 
