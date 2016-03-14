1be9df46c2ab6c98d211a09c4ff4f7fc14a55c4d
dragsubil <subil1407@gmail.com>
2016-03-14 18:45:21 +0530

    started work on replacing chapter links in each page

--- pagesaver/pagesaver.py
+++ pagesaver/pagesaver.py
@@ -4,6 +4,8 @@ import urllib.request
 
 #function to extract link and name from the line
 
+prev_name_name=''        
+
 def linkExtract(file_line):
 
 	pattern1=re.compile(r'''		#regex patteren to seperate link, any tag inbetween link and name, and name
@@ -19,12 +21,12 @@ def linkExtract(file_line):
 			''', re.VERBOSE)        
 			
 	try:
-		link_and_name_tup=pattern1.search(file_line).groups()  #groups forms a tuple containing the elements obtained in the parantheses of the regex
+		link_and_name_tup=pattern1.search(file_line).groups()   #groups forms a tuple containing the elements obtained in the parantheses of the regex
 		
 	except AttributeError:										#in case of a line with none of the properties in regex, None is obtained in pattern.search
 		return													#this raises AttributeError, so we must got to the next line, hence return a None
 		
-	print(link_and_name_tup)
+	#print(link_and_name_tup)
 	link1=link_and_name_tup[0]
 	name1=link_and_name_tup[2]									#name is actually the third element, second being a potential inbetween tag
 	link1=link1[1:-1]						   				 	#removes the unwanted quotes from the ends of the link we got 
@@ -40,21 +42,50 @@ def fileParse(link_file):
 		except TypeError:									
 			continue
 			
-		print(link1,name1)
-		pageSave(link1,name1)                            #passes link and name to pageSave function
+		#print(link1,name1)
+		pageSave(link1,name1)                            	    #passes link and name to pageSave function
 
 		
+def linkReplace(new_file_name):									#to find and replace hyperlinks at the "previous chapter" and "next chapter" positions
+	if prev_name_name=='':										#seperate linkReplace and linkReplaceProper for readability
+		prev_name_name=new_file_name
+	else:
+		linkReplaceProper(prev_name_name,new_file_name)
+		prev_name_name=new_file_name
+		
+		
+def linkReplaceProper(prev_name,next_name):
+	with open(prev_name,'a+') as file1:
+		file1.seek(0)
+		linkReplaceRegex(file1,next_name)
+	with open(next_name,'a+') as file2
+		file2.seek(0)
+		linkReplaceRegex(file2,prev_name)
+		
+def linkReplaceRegex(file_obj,file_name):
+	for i in file_obj:
+		
+	
+	
+	
+	
+	
+
+	
 #function to save the page with the extracted name, given by the extracted link 
 def pageSave(page_link,page_name):
 	file_name="pages/{}.html".format(page_name)              
 	with urllib.request.urlopen(page_link) as page_object:
-		with open(file_name,'ab+') as file1:
+		with open(file_name,'wb+') as file1:
 			file1.write(page_object.read())
+			print('Downloaded: {}.html'.format(page_name))
+	linkReplace(file_name)
+			
 			
 
 #combined unit test
 
-with open('tablesample.html','r+') as file1:
+with open('table.html','r+') as file1:
 	fileParse(file1)
 
 
