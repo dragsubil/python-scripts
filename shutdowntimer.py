aecbf7bf7f83e7b2ed8bf2384a280a1d2a437df0
dragsubil <subil1407@gmail.com>
2016-03-25 22:33:53 +0530

    merged working-stuff. changes in git ignore

--- shutdowntimer.py
+++ shutdowntimer.py
@@ -8,21 +8,25 @@ import os
 
 print("-----SHUTDOWN TIMER-----")
 
-a=input("Press S to schedule shutdown.\nPress C to cancel shutdown:\n")
 
-if(a=='s' or a=='S'):
 
-	hours=int(input("\n\nEnter hours: "))
-	minutes=int(input("Enter minutes: "))
+while(True):
+	a=input("Press S to schedule shutdown.\nPress C to cancel shutdown.\n")
+	if(a=='s' or a=='S'):
 
-	seconds=hours*60*60+minutes*60
+		hours=int(input("\n\nEnter hours: "))
+		minutes=int(input("Enter minutes: "))
 
-	os.system('shutdown -s -t {}'.format(seconds))
-	print("Your system will shutdown in {} hours and {} minutes".format(hours,minutes))
+		seconds=hours*60*60+minutes*60
 
-elif(a=='C' or a=='c'):
-	os.system('shutdown -a')
-	
-else:
-	print("Sorry. Try again.")
+		os.system('shutdown -s -t {}'.format(seconds))
+		print("Your system will shutdown in {} hours and {} minutes".format(hours,minutes))
+		break
+
+	elif(a=='C' or a=='c'):
+		os.system('shutdown -a')
+		break
+		
+	else:
+		print("Sorry. Try again.")
 	
