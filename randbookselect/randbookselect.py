2441ffd633a0b0af5b188bef770ab5a6c9328daa
dragsubil <subil1407@gmail.com>
2016-05-18 12:31:06 +0530

    I don't know what changes I made but I'm sure its fine

--- randbookselect/randbookselect.py
+++ randbookselect/randbookselect.py
@@ -1,3 +1,7 @@
+# Copies a rndom book from your book folder into your current folder
+
+
+
 import os
 import random
 import re
@@ -13,7 +17,7 @@ def getbooks(directory):
     for i in dir_list:
         path1 = os.path.join(directory, i)
         if os.path.isfile(path1):
-        	#This ignores any other file formats except the ebook file formats
+            # This ignores any other file formats except the ebook file formats
             if re.search(r'.*(.pdf|.epub|.mobi)', i):
                 booklist.append(i)
                 bookpath.append(path1)
@@ -26,5 +30,5 @@ rand_number = random.randrange(len(booklist))
 rand_book_path = bookpath[rand_number]
 rand_book_name = booklist[rand_number]
 
-#copies the selected book from book folder to current directory
+# Copies the selected book from book folder to current directory
 copy2(rand_book_path, rand_book_name)
